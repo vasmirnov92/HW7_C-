c503fd5bda8b91f9663d4b2e72267b3174060851
Vasiiy <vasya.smirnoff@mail.ru>
2023-02-05 01:05:14 +0300

    initial commit wrigth

---
+++ task47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble();
+            result[i, j] = result[i, j] * 100;
+            result[i, j] = Math.Round(result[i, j], 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t "); // табуляция
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(row, col, 0, 10);
+PrintArray(array);
